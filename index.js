ea9e2d568d1c5ccefbbcce02e60a765cc8429e1e
Alvy <alvyahmed03@gmail.com>
2025-09-09 19:50:37 +0600

    Details modal added

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ console.log("JS connected");
 
 const getInBtn = document.getElementById("getin");
 getInBtn.addEventListener('click', () => {
-    addSelected();
+   // addSelected();
+    //plantDetail(3);
     console.log("get in clicked")
 })
 
@@ -97,7 +98,7 @@ const getPlants = async () => {
         newPlant.innerHTML = `
             <img class="w-[150px] h-[70px] md:w-[300px] md:h-[175px]" src="${plants[i].image}" alt=${plants[i].name} />
             <div class="flex flex-col gap-2">
-                <span class="text-md md:text-xl font-bold cursor-pointer">
+                <span class="name text-md md:text-xl w-max font-bold cursor-pointer">
                     ${plants[i].name}
                 </span>
                 <span class="text-sm md:text-md text-gray-500">
@@ -120,14 +121,93 @@ const getPlants = async () => {
         cardsContainer.appendChild(newPlant);
     }
 
+    document.querySelectorAll('.name').forEach((name, index) => {
+        name.addEventListener('click', () => {
+            const card = name.parentElement.parentElement;
+
+            // const imgSrc = card.children[0].src;
+            // const nameText = card.children[1].children[0].innerText;
+            // const des = card.children[1].children[1].innerText;
+            // const type = card.children[1].children[2].children[0].innerText;
+            // const price = parseInt(card.children[1].children[2].children[1].children[0].innerText);
+            const plantId = plants[index].id;
+            plantDetail(plantId);
+            //console.log(index+1)
+            //console.log(imgSrc, nameText, des, type, price)
+
+        })
+    })
+
     document.querySelectorAll('.addCartBtn').forEach((btn) => {
         btn.addEventListener('click', () => {
+            // Add to cart
             const card = btn.parentElement;
+            const cartSM = document.getElementById('smCart');
+            const cartMD = document.getElementById('mdCart');
+            //console.log(cartMD, cartSM);
             let price = parseInt(card.querySelector(".price").innerText)
             let newPrice = parseInt(total[0].innerText) + price;
+
             total[0].innerText = newPrice;
             total[1].innerText = newPrice;
-            console.log(total[0].innerText);
+
+            const name = card.querySelector('.name').innerText;
+            const prc = card.querySelector('.price').innerText;
+            
+            const newOrder = document.createElement('div');
+            newOrder.className = "flex justify-between items-center h-[50px] gap-1 px-[7px] md:px-[12px] bg-[#DCFCE7] rounded-xl w-full";
+
+            newOrder.innerHTML = `
+                <div class="flex flex-col gap-1">
+                    <span class="text-[10px] md:text-[15px]">
+                        ${name}
+                    </span>
+                    <span class="text-xs md:text-sm text-gray-500">
+                        ${price}
+                    </span>
+                </div>
+                <span class="remove text-xs md:text-sm text-gray-500 cursor-pointer">
+                    ❌
+                </span>
+            `
+
+            const newOrderClone = newOrder.cloneNode(true);
+            cartSM.appendChild(newOrder);
+            cartMD.appendChild(newOrderClone);
+
+            const rmvBtn = newOrder.querySelector('.remove');
+            rmvBtn.addEventListener('click', () => {
+                let newPrice = parseInt(total[0].innerText) - price;
+
+                total[0].innerText = newPrice;
+                total[1].innerText = newPrice;
+
+                const cartSM = document.getElementById('smCart');
+                const cartMD = document.getElementById('mdCart');
+
+                
+                cartSM.removeChild(newOrder);
+                cartMD.removeChild(newOrderClone);
+                //console.log('remove clicked');
+            })
+
+            const rmvBtnClone = newOrderClone.querySelector('.remove');
+            rmvBtnClone.addEventListener('click', () => {
+                let newPrice = parseInt(total[0].innerText) - price;
+
+                total[0].innerText = newPrice;
+                total[1].innerText = newPrice;
+
+                const cartSM = document.getElementById('smCart');
+                const cartMD = document.getElementById('mdCart');
+
+                
+                cartSM.removeChild(newOrder);
+                cartMD.removeChild(newOrderClone);
+                //console.log('remove clicked');
+            })
+            //console.log(newOrder);
+            //console.log(total[0].innerText);
             //console.log(price)
         })
     })
@@ -157,7 +237,7 @@ const getPlantsCat = async (id) => {
         newPlant.innerHTML = `
             <img class="w-[150px] h-[70px] md:w-[300px] md:h-[175px]" src="${plantsCat[i].image}" alt=${plantsCat[i].name} />
             <div class="flex flex-col gap-2">
-                <span class="text-md md:text-xl font-bold cursor-pointer">
+                <span class="name text-md md:text-xl font-bold cursor-pointer">
                     ${plantsCat[i].name}
                 </span>
                 <span class="text-sm md:text-md text-gray-500">
@@ -172,22 +252,94 @@ const getPlantsCat = async (id) => {
                     </span>
                 </div>
             </div>
-            <button class="text-[12px] md:text-sm text-white bg-green-700 hover:bg-green-500 w-full h-[25px] md:h-[30px] rounded-4xl cursor-pointer">
+            <button class="addCartBtn text-[12px] md:text-sm text-white bg-green-700 hover:bg-green-500 w-full h-[25px] md:h-[30px] rounded-4xl cursor-pointer">
                 Add to Cart
             </button>
         `
 
-        document.querySelectorAll('.addCartBtn').forEach((btn) => {
-            btn.addEventListener('click', () => {
-                const card = btn.parentElement;
-                let price = parseInt(card.querySelector(".price").innerText);
-                let newPrice = parseInt(total.innerText) + price;
-                total.innerText = newPrice;
-                console.log("Added:", price, "New total:", newPrice);
-            });
-        });
         cardsContainer.appendChild(newPlant);
     }
+
+    document.querySelectorAll('.name').forEach((name, index) => {
+        name.addEventListener('click', () => {
+            const plantId = plantsCat[index].id; 
+            plantDetail(plantId);
+        })
+    })
+
+    document.querySelectorAll('.addCartBtn').forEach((btn) => {
+        btn.addEventListener('click', () => {
+            // Add to cart
+            const card = btn.parentElement;
+            const cartSM = document.getElementById('smCart');
+            const cartMD = document.getElementById('mdCart');
+            //console.log(cartMD, cartSM);
+            let price = parseInt(card.querySelector(".price").innerText)
+            let newPrice = parseInt(total[0].innerText) + price;
+
+            total[0].innerText = newPrice;
+            total[1].innerText = newPrice;
+
+            const name = card.querySelector('.name').innerText;
+            const prc = card.querySelector('.price').innerText;
+            
+            const newOrder = document.createElement('div');
+            newOrder.className = "flex justify-between items-center h-[50px] gap-1 px-[7px] md:px-[12px] bg-[#DCFCE7] rounded-xl w-full";
+
+            newOrder.innerHTML = `
+                <div class="flex flex-col gap-1">
+                    <span class="text-[10px] md:text-[15px]">
+                        ${name}
+                    </span>
+                    <span class="text-xs md:text-sm text-gray-500">
+                        ${price}
+                    </span>
+                </div>
+                <span class="remove text-xs md:text-sm text-gray-500 cursor-pointer">
+                    ❌
+                </span>
+            `
+
+            const newOrderClone = newOrder.cloneNode(true);
+            cartSM.appendChild(newOrder);
+            cartMD.appendChild(newOrderClone);
+
+            const rmvBtn = newOrder.querySelector('.remove');
+            rmvBtn.addEventListener('click', () => {
+                let newPrice = parseInt(total[0].innerText) - price;
+
+                total[0].innerText = newPrice;
+                total[1].innerText = newPrice;
+
+                const cartSM = document.getElementById('smCart');
+                const cartMD = document.getElementById('mdCart');
+
+                
+                cartSM.removeChild(newOrder);
+                cartMD.removeChild(newOrderClone);
+                //console.log('remove clicked');
+            })
+
+            const rmvBtnClone = newOrderClone.querySelector('.remove');
+            rmvBtnClone.addEventListener('click', () => {
+                let newPrice = parseInt(total[0].innerText) - price;
+
+                total[0].innerText = newPrice;
+                total[1].innerText = newPrice;
+
+                const cartSM = document.getElementById('smCart');
+                const cartMD = document.getElementById('mdCart');
+
+                
+                cartSM.removeChild(newOrder);
+                cartMD.removeChild(newOrderClone);
+                //console.log('remove clicked');
+            })
+            //console.log(newOrder);
+            //console.log(total[0].innerText);
+            //console.log(price)
+        })
+    })
 
     cardLoading.classList.add('hidden');
     cardsContainer.classList.remove('hidden');
@@ -218,3 +370,24 @@ const addSelected = (category) => {
     //console.log(catCollMD);
 }
 
+// Display modal
+const plantDetail = async (id) => {
+    let plant = [];
+    const res = await fetch(`https://openapi.programming-hero.com/api/plant/${id}`);
+    const data = await res.json();
+    plant = data.plants;
+
+    const detailBox = document.getElementById('detailBox');
+    detailBox.innerHTML = `
+        <img class="w-[250px] h-[175px] md:w-[550px] md:h-[375px]" src=${plant.image} alt="plant" />
+        <div class="flex flex-col mt-4 gap-2">
+            <span class="font-bold">${plant.name}</span>
+            <span class="text-sm text-gray-500">${plant.description}</span>
+            <span>৳${plant.price}</span>
+        </div>
+    `
+
+    const modal = document.getElementById('detail').showModal();
+    console.log(data.plants);
+
+}
